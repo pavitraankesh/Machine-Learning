c7e3f59d4c1aa7bffbb943e5b326c5eea142213a
pavitraankesh <70202515+pavitraankesh@users.noreply.github.com>
2021-08-30 11:52:14 +0530

    Create RamdomForest.py

---
+++ Programs/RamdomForest.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+data = pd.read_csv('heart disease.csv')
+x = data.drop("target", axis=1)
+y = data["target"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
+classifier=RandomForestClassifier(n_estimators=10,criterion = 'entropy', random_state = 0)
+classifier.fit(x_train, y_train)
+res= classifier.predict(x_test)
+print(res)
